0219104c0b937f215e4f336426ada0ffdcd04d3d
Mareks Robalds <robaldsmareks@gmail.com>
2022-12-07 00:31:37 +0200

    Updated file till page 40 of pdf

---
+++ src/app/Http/Controllers/BookController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Requests\BookRequest;
+
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function list()
+    {
+        $items = Book::orderBy('name', 'asc')->get();
+        return view(
+            'book.list',
+            [
+                'title' => 'Grāmatas',
+                'items' => $items
+            ]
+        );
+    }
+
+    public function create()
+    {
+        $authors = Author::orderBy('name', 'asc')->get();
+        return view(
+            'book.form',
+            [
+                'title' => 'Pievienot grāmatu',
+                'book' => new Book(),
+                'authors' => $authors,
+            ]
+        );
+    }
+    private function saveBookData(Book $book, BookRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $book->fill($validatedData);
+        $book->display = (bool) ($validatedData['display'] ?? false);
+        if ($request->hasFile('image')) {
+            $uploadedFile = $request->file('image');
+            $extension = $uploadedFile->clientExtension();
+            $name = uniqid();
+            $book->image = $uploadedFile->storePubliclyAs(
+                '/',
+                $name . '.' . $extension,
+                'uploads'
+            );
+        }
+        $book->save();
+    }
+    public function put(BookRequest $request)
+    {
+        $book = new Book();
+        $this->saveBookData($book, $request);
+        return redirect('/books');
+    }
+    public function patch(Book $book, BookRequest $request)
+    {
+        $this->saveBookData($book, $request);
+        return redirect('/books/update/' . $book->id);
+    }
+
+    public function update(Book $book)
+    {
+        $authors = Author::orderBy('name', 'asc')->get();
+        return view(
+            'book.form',
+            [
+                'title' => 'Rediģēt grāmatu',
+                'book' => $book,
+                'authors' => $authors,
+            ]
+        );
+    }
+
+    public function delete(Book $book)
+    {
+        $book->delete();
+        return redirect('/books');
+    }
+}
---
+++ src/app/Models/Book.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Book extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'author_id', 'description', 'price', 'year'];
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id');
+            $table->string('name', 256);
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2)->nullable();
+            $table->integer('year');
+            $table->string('image', 256)->nullable();
+            $table->boolean('display');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
